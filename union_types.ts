7a1746144b0695d869375e0a5254fd23c79fae6d
Sagar Dighe <sagasoftech@gmail.com>
2023-05-10 23:55:59 -0500

    Rename variable

--- union_types.ts
+++ union_types.ts
@@ -1,7 +1,7 @@
 
 
-let course: string | number = 'My TypeScript course';
+let course_union: string | number = 'My TypeScript course';
 
 
 //Below will be allowed
-course = 123;
+course_union = 123;
