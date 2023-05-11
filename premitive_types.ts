1c9fb797488fac529dabd032332fbc47e70a21f8
Sagar Dighe <sagasoftech@gmail.com>
2023-05-10 23:07:41 -0500

    Premitive types: number, string, boolean

---
+++ premitive_types.ts
@@ -0,0 +1,17 @@
+//Premitives
+/*
+They must start with lower case otherwise they become
+Object in TypeScript
+*/
+
+//integer or float
+let age: number =24;
+age = 10.5;
+
+//string
+let userName: string;
+userName = "sagar";
+
+//boolean
+let isInstructor: boolean;
+isInstructor=true;
