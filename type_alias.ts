f2d6bd2858d4bf44aed0f12dad860af7264f48d6
Sagar Dighe <sagasoftech@gmail.com>
2023-05-11 00:08:11 -0500

    Defning Type Alias

---
+++ type_alias.ts
@@ -0,0 +1,26 @@
+
+//Type Alias
+
+type Person = {
+    name: string;
+    age: number;
+};
+
+
+//Usage
+
+let person1: Person;
+person1 = {
+    name: 'sagar',
+    age: 35
+}
+
+let person2: Person;
+person2 = {
+    name: 'sagar',
+    age: 35
+}
+
+let persons: Person[];
+persons = [person1, person2];
+
